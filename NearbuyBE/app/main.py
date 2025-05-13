c5fe78dca601435894adbb7e584dae2592a7ef97
michaling <michalling@gmail.com>
2025-05-13 21:07:25 +0300

    updated

--- NearbuyBE/app/main.py
+++ NearbuyBE/app/main.py
@@ -4,8 +4,3 @@ from app.auth.routes import router as auth_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World2"}
