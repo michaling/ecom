c9cb72cee1ff8170543b0f8ce0604ce090b7035a
Lilachay <lilach910@gmail.com>
2025-06-02 16:27:25 +0300

    added "add" option

--- NearBuyFE/app/(dashboard)/(list)/listScreen.tsx
+++ NearBuyFE/app/(dashboard)/(list)/listScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import Checklist from '@/components/Checklist';
 import RecommendationItem from '@/components/RecommendationItem';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 export default function ListScreen() {
   const { id, title, color, items: listItems, suggestions: rawSuggestions } = useLocalSearchParams();
@@ -10,6 +11,8 @@ export default function ListScreen() {
 
   const [items, setItems] = useState<any[]>([]);
   const [recommended, setRecommended] = useState<string[]>([]);
+  const [isAdding, setIsAdding] = useState(false);
+  const [newItemName, setNewItemName] = useState('');
   
   useEffect(() => {
     if (listItems) {
@@ -49,6 +52,19 @@ export default function ListScreen() {
     setItems(prev => prev.filter(item => item.id !== id));
   };
 
+  const addNewItem = () => {
+    if (newItemName.trim() === '') return;
+    const newItem = {
+      id: Date.now().toString(),
+      name: newItemName,
+      isChecked: false,
+    };
+    setItems(prev => [...prev, newItem]);
+    setNewItemName('');
+    setIsAdding(false);
+  };
+
+
   const handleAddRecommendation = (name: string) => {
     const newItem = {
       id: Date.now().toString(),
@@ -61,6 +77,7 @@ export default function ListScreen() {
 
   return (
     <View style={styles.container}>
+     
       <View style={[styles.header, { backgroundColor: background || '#E6E6FA' }]}>
         <Text style={styles.title}>{title} </Text>
         <Text style={styles.subtitle}>
@@ -69,12 +86,42 @@ export default function ListScreen() {
       </View>
   
       <View style={styles.content}>
-        <Checklist
-          items={items}
-          onToggle={toggleItem}
-          onNameChange={changeItemName}
-          onDelete={deleteItem}
-        />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
+        <View style={styles.topContent}> 
+          <View style={{ flexGrow: 0 }}> 
+            <Checklist
+              items={items}
+              onToggle={toggleItem}
+              onNameChange={changeItemName}
+              onDelete={deleteItem}
+            />
+          </View>
+          <View style={styles.addContainer}> 
+            {!isAdding && (
+              <TouchableOpacity style={styles.addButton}
+                onPress={() => setIsAdding(true)}
+              >
+                <AntDesign name="plus" size={33} color="purple" />
+                {/*<Text style={styles.addButtonText}>Add Item</Text> */}
+              </TouchableOpacity>
+            )}
+            {isAdding && (
+              <TextInput
+                style={styles.input}
+                value={newItemName}
+                onChangeText={setNewItemName}
+                placeholder="Enter item name"
+                onSubmitEditing={addNewItem}
+                onBlur={addNewItem}
+                autoFocus
+              />
+            )}
+          </View>
+      </View>
+      </KeyboardAvoidingView>
       {recommended.length > 0 && (
           <View style={styles.recommendations}>
             <Text style={styles.recommendTitle}>Recommended for you</Text>
@@ -87,6 +134,7 @@ export default function ListScreen() {
             ))}
           </View>
         )}
+
       </View>
     </View>
   );
@@ -118,7 +166,7 @@ const styles = StyleSheet.create({
 
   content: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FAFAFA',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     marginTop: -15,
@@ -133,7 +181,44 @@ const styles = StyleSheet.create({
     // Shadow (Android)
     elevation: 10,
   },
-  
+  topContent: {
+    flexGrow: 1,
+  },
+
+  addContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+
+  addButton: {
+    borderWidth: 1,
+    borderColor: "purple",
+    padding: 12,
+    marginTop: 12,
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addButtonText: {
+    fontSize: 12,
+    color: '#333',
+    marginTop: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+    fontSize: 16,
+  },
+
+  recommendationsBox: {
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    paddingTop: 12,
+  },
+
   recommendations: {
     marginTop: 24,
     marginBottom: 50,
@@ -144,4 +229,5 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingLeft: 4,
   },
+  
 });
