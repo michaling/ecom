c21efa90f501aa35a12ff4fecdec92adbac57100
michaling <michalling@gmail.com>
2025-06-06 18:46:26 +0300

    wip

--- NearbuyBE/app/supabase_client.py
+++ NearbuyBE/app/supabase_client.py
@@ -1,4 +1,5 @@
 from supabase import create_client
+from supabase.client import ClientOptions
 import os
 from dotenv import load_dotenv
 import httpx
@@ -8,4 +9,9 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
-supabase = create_client(url, key)
+
+supabase = create_client(url, key, options=ClientOptions(
+        postgrest_client_timeout=10,
+        storage_client_timeout=10,
+        schema="public",
+    ))
