1c5e912b1646e3ffed4ec51b7ae2798987756779
michal ling <55466112+michaling@users.noreply.github.com>
2025-08-02 23:01:46 +0300

    Delete test_import.py

--- test_import.py
+++
@@ -1,6 +0,0 @@
-# test_import.py
-from ml_component.locations_based_recommender_agent.product_availability_agent import (
-    ProductAvailabilityAgent,
-)
-
-print("Import successful:", ProductAvailabilityAgent)
